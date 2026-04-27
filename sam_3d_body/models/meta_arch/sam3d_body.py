aed67324da4c40b734bb1ca0f85f04e76b644cff
aliciusschroeder <alicius.schroeder@gmail.com>
2026-04-27 17:30:10 +0200

    fix: save pred_pose_raw from body pass before it gets zeroed by the re-run

--- sam_3d_body/models/meta_arch/sam3d_body.py
+++ sam_3d_body/models/meta_arch/sam3d_body.py
@@ -1589,6 +1589,9 @@ class SAM3DBody(BaseModel):
 
         ########################################################
 
+        # Save pred_pose_raw from body pass before it gets zeroed by the re-run
+        pred_pose_raw_body = pose_output["mhr"]["pred_pose_raw"].detach().clone()
+
         # Re-run forward
         with torch.no_grad():
             verts, j3d, jcoords, mhr_model_params, joint_global_rots = (
@@ -1613,8 +1616,8 @@ class SAM3DBody(BaseModel):
             pose_output["mhr"]["pred_keypoints_3d"] = j3d
             pose_output["mhr"]["pred_vertices"] = verts
             pose_output["mhr"]["pred_joint_coords"] = jcoords
-            pose_output["mhr"]["pred_pose_raw"][...] = (
-                0  # pred_pose_raw is not valid anymore
+            pose_output["mhr"]["pred_pose_raw"] = (
+                pred_pose_raw_body  # preserve body-pass pred_pose_raw
             )
             pose_output["mhr"]["mhr_model_params"] = mhr_model_params
 
